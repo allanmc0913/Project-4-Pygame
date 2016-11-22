16d4dfdcecd5d2e048019f601ca43e97e9358fed
Allan Chen <allanmc@umich.edu>
2016-11-22 08:29:33 -0500

    3rd day commit, added more changes to main.py file

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-import 
+#!/usr/bin/env python
+
+import pygame
+import sys
+import time
+import random
+
+from pygame.locals import *
+
+FPS = 20
+#launch pygame
+pygame.init()
+
+#create an object to track time
+fpsClock=pygame.time.Clock()
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+surface = pygame.Surface(screen.get_size())
+surface = surface.convert()
+surface.fill((255,255,255))
+clock = pygame.time.Clock()
+
+
+pygame.key.set_repeat(1, 40)
+
+GRIDSIZE=10
+GRID_WIDTH = SCREEN_WIDTH / GRIDSIZE
+GRID_HEIGHT = SCREEN_HEIGHT / GRIDSIZE
+
+#x and y values for keyboard
+UP    = (0, -1)
+DOWN  = (0, 1)
+LEFT  = (-1, 0)
+RIGHT = (1, 0)
+    
+screen.blit(surface, (0,0))
+
+class Food(object):
+	def __init__(self):
+		self.color(10,0,0)
